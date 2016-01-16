e37a852a554bae40b53292e43b65cb8d81fc1cb2
Kirby <knoverman@comcast.net>
2016-01-16 15:36:01 -0700

    classes

---
+++ src/carbohidrati_italiano/robots/Guard.java
@@ -0,0 +1,17 @@
+package carbohidrati_italiano.robots;
+
+import battlecode.common.RobotController;
+
+public class Guard extends RobotBase{
+
+	@Override
+	public void doWork(RobotController rc) throws Exception {
+		
+	}
+
+	@Override
+	public void init(RobotController rc) throws Exception {
+		
+	}
+
+}
---
+++ src/carbohidrati_italiano/robots/Viper.java
@@ -0,0 +1,19 @@
+package carbohidrati_italiano.robots;
+
+import battlecode.common.RobotController;
+
+public class Viper extends RobotBase{
+
+	@Override
+	public void doWork(RobotController rc) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(RobotController rc) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
