2eb4239fb69753e4f4faf5ffb6e6d7fd74acd165
Matt <MrZoraman@Gmail.com>
2016-01-16 15:44:43 -0700

    DoNothingRobot added

---
+++ src/doNothingRobot/RobotPlayer.java
@@ -0,0 +1,13 @@
+package doNothingRobot;
+
+import battlecode.common.*;
+
+import java.util.Random;
+
+public class RobotPlayer {
+    public static void run(RobotController rc) {
+    	while(true) {
+    		Clock.yield();
+    	}
+    }
+}
