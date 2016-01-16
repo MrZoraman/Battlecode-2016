f12544d8e5722b8ba17b8a0eaf2c8a9d36bb6b9c
Matt <MrZoraman@Gmail.com>
2016-01-16 13:46:14 -0700

    Our robot started

---
+++ src/carbohidrati_italiano/RobotPlayer.java
@@ -0,0 +1,12 @@
+package carbohidrati_italiano;
+
+import battlecode.common.Clock;
+import battlecode.common.RobotController;
+
+public class RobotPlayer {
+	public static void run(RobotController rc) {
+		while(true) {
+			Clock.yield();
+		}
+	}
+}
