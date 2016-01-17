1f0c81aa4c524966e8a595b80a74b434a08405d4
Matt <MrZoraman@Gmail.com>
2016-01-17 15:22:33 -0700

    The pathfinder is periodically updated

--- src/carbohidrati_italiano/robots/RobotBase.java
+++ src/carbohidrati_italiano/robots/RobotBase.java
@@ -1,8 +1,12 @@
 package carbohidrati_italiano.robots;
 
 import battlecode.common.Clock;
+import battlecode.common.Direction;
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import battlecode.common.Team;
+import carbohidrati_italiano.Globals;
+import carbohidrati_italiano.dstarlite.DStarLite;
 
 public abstract class RobotBase{
 	
@@ -16,6 +20,9 @@ public abstract class RobotBase{
 	protected Team enemyTeam;
 	
 	private Goal currentGoal;
+	private final boolean updatePathFinder;
+	
+	private final DStarLite pf = new DStarLite();
 	
 	public final void run(RobotController rc) {
 		try {
@@ -24,6 +31,7 @@ public abstract class RobotBase{
 			updateGoalString(rc, currentGoal);
 			init(rc);
 			while(true) {
+				updatePathFinder(rc);
 				Goal newGoal = currentGoal.achieveGoal(rc, this);
 				if(newGoal != null) {
 					currentGoal = newGoal;
@@ -40,5 +48,24 @@ public abstract class RobotBase{
 	
 	private void updateGoalString(RobotController rc, Goal goal) {
 		rc.setIndicatorString(0, "Goal: " + goal.getName());
+	}
+	
+	protected void updatePathFinder(RobotController rc) throws Exception {
+		MapLocation myLocation = rc.getLocation();
+		for(Direction dir : Globals.movableDirections) {
+			MapLocation newLocation = myLocation.add(dir);
+			if(!rc.onTheMap(newLocation)) {
+				pf.updateCell(newLocation.x, newLocation.y, -1);
+				continue;
+			}
+			double rubble = rc.senseRubble(newLocation);
+			if(rubble > 100.0) {
+				pf.updateCell(newLocation.x, newLocation.y, -1);
+			}
+		}
+	}
+	
+	public DStarLite getPathFinder() {
+		return pf;
 	}
 }
