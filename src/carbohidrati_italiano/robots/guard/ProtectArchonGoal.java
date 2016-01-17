de8a78665f97712b1c667c34e4731eb2519988c0
Matt <MrZoraman@Gmail.com>
2016-01-17 13:39:50 -0700

    Guards know where their archon is

--- src/carbohidrati_italiano/robots/guard/ProtectArchonGoal.java
+++ src/carbohidrati_italiano/robots/guard/ProtectArchonGoal.java
@@ -1,15 +1,42 @@
 package carbohidrati_italiano.robots.guard;
 
 import battlecode.common.RobotController;
+import battlecode.common.Signal;
 import carbohidrati_italiano.robots.Goal;
 import carbohidrati_italiano.robots.RobotBase;
+import carbohidrati_italiano.robots.Signals;
 
 public class ProtectArchonGoal implements Goal {
+	
+	private int archonId = -1;
 
 	@Override
 	public Goal achieveGoal(RobotController rc, RobotBase robot) throws Exception {
+		Signal[] signals = rc.emptySignalQueue();
 		
-		
+		for(Signal s : signals) {
+			int[] messages = s.getMessage();
+			if(messages == null) {
+				continue;
+			}
+			
+			Signals id = Signals.toSignal(messages[0]);
+			if(id == null) {
+				System.out.println("continuing 27");
+				continue;
+			}
+			
+			switch(id) {
+			case THIS_IS_MY_ID:
+				if(archonId < 0) {
+					archonId = messages[1];
+					rc.setIndicatorString(1, "My Archon is at: " + archonId);
+				}
+				break;
+			default:
+				continue;
+			}
+		}
 		
 		return null;
 	}
