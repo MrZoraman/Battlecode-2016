0596bfae69e966641ad0648d77c6878c875c61fa
Matt <MrZoraman@Gmail.com>
2016-01-17 13:26:56 -0700

    Added signals

---
+++ src/carbohidrati_italiano/robots/Signals.java
@@ -0,0 +1,32 @@
+package carbohidrati_italiano.robots;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Signals {
+	
+	HERE_I_AM(1);
+	
+	private static Map<Integer, Signals> signalsMap = new HashMap<>();
+	
+	static {
+		Signals[] arrayOfSignals = Signals.values();
+		for(Signals s : arrayOfSignals) {
+			signalsMap.put(s.getValue(), s);
+		}
+	}
+	
+	public static Signals toSignal(int value) {
+		return signalsMap.get(value);
+	}
+	
+	private int value;
+	
+	private Signals(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
