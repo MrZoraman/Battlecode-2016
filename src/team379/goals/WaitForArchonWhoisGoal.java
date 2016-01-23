c2e401c5af1faea34a0dee97a1935ec54c9174ac
Matt <MrZoraman@Gmail.com>
2016-01-22 18:55:10 -0700

    WFAWG uses the HAI

--- src/team379/goals/WaitForArchonWhoisGoal.java
+++ src/team379/goals/WaitForArchonWhoisGoal.java
@@ -1,10 +1,8 @@
 package team379.goals;
 
-import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
-import battlecode.common.RobotInfo;
+import team379.HeadArchonIdentifier;
 import team379.signals.SignalReader;
-import team379.signals.SignalType;
 
 /**
  * Waits for a notication from and archon containing the id. It then finds the location of the archon,
@@ -13,49 +11,21 @@ import team379.signals.SignalType;
  *
  */
 public class WaitForArchonWhoisGoal implements Goal {
-	
-	/**
-	 * The archon id. -1 means no archon id has been found yet.
-	 */
-	private int archonId = -1;
 
 	@Override
 	public Goal achieveGoal(RobotController rc) throws Exception {
+		HeadArchonIdentifier hai = new HeadArchonIdentifier();
 		//read the signals to see if the archonId has come through
-		SignalReader.consume(rc, data -> {
-			if(data.getType() == SignalType.THIS_IS_MY_ID) {
-				archonId = data.getOtherInfo();
-			}
-		});
+		SignalReader.consume(rc, hai);
 		
-		if(archonId < 0) {
+		if(hai.getArchonLocation() == null) {
 			//no archon id yet, try again next turn.
 			return null;
 		}
 		
-		//I know what my archon id is!
-		
-		//get friendly robots in range
-		RobotInfo[] nearbyRobots = rc.senseNearbyRobots(rc.getType().sensorRadiusSquared, rc.getTeam());
-		
-		//the location of my archon
-		MapLocation archonLocation = null;
-		
-		for(RobotInfo ri : nearbyRobots) {
-			if(ri.ID == archonId) {
-				archonLocation = ri.location;
-			}
-		}
-		
-		if(archonLocation == null) {
-			//I didn't find my archon in range... I have no idea what to do so I'm going to wait here. Maybe I'll
-			//sense the archon next round. (In theory flow control should never get to this point)
-			return null;
-		}
-		
-		//I know where my archon is!
+		//I konw who and where my archon is!
 		//Time to let the goal factory do the rest...
-		return GoalFactory.createGoal(rc, archonLocation);
+		return GoalFactory.createGoal(rc, hai.getArchonLocation());
 	}
 
 	@Override
