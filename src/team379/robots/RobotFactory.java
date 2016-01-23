8edffc2b5fe323e7620d15e14c8d0dd756e435ab
Matt <MrZoraman@Gmail.com>
2016-01-22 18:11:09 -0700

    Significantly changed the structure of the RobotFactory.

--- src/team379/robots/RobotFactory.java
+++ src/team379/robots/RobotFactory.java
@@ -1,48 +1,42 @@
 package team379.robots;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import battlecode.common.RobotType;
-import team379.Globals;
-import team379.goals.EmptyGoal;
-import team379.goals.PatrolAroundArchonGoal;
-import team379.goals.WaitForArchonWhoisGoal;
-import team379.goals.archon.BeginningBuildGoal;
-import team379.goals.scout.ScoutPatrolGoal;
-import team379.goals.turret.MoveAwayGoal;
 
-import static battlecode.common.RobotType.*;
-
+/**
+ * Creates a robot of a specific type.
+ * @author Matt
+ *
+ */
 public class RobotFactory {
-	private static final Map<RobotType, Robot> robots = new HashMap<>();
-	
-	static {
-		robots.put(ARCHON, new Robot(new BeginningBuildGoal()));
-		
-		robots.put(GUARD, new Robot(
-				new WaitForArchonWhoisGoal(Globals.GUARD_AGGRESSION_RANGE, GUARD,
-						mem -> new PatrolAroundArchonGoal(mem))));
-		
-		robots.put(SCOUT, new Robot(
-				new WaitForArchonWhoisGoal(0, SCOUT, mem -> new ScoutPatrolGoal(mem))));
-		
-		robots.put(SOLDIER, new Robot(
-				new WaitForArchonWhoisGoal(Globals.SOLDIER_AGGRESSION_RANGE, SOLDIER, 
-						mem -> new PatrolAroundArchonGoal(mem))));
-		
-		robots.put(TTM, new Robot(new EmptyGoal()));
-		
-		robots.put(TURRET, new Robot(
-				new WaitForArchonWhoisGoal(0, TURRET,
-						mem -> new MoveAwayGoal(mem))));
-		
-		robots.put(VIPER, new Robot(new EmptyGoal()));
-	}
-	
+	/**
+	 * Constructs and returns a Robot given the type of that robot.
+	 * @param type The type of the robot.
+	 * @return A Robot with the proper setup completed.
+	 */
 	public static Robot getRobot(RobotType type) {
-		return robots.get(type);
+		switch(type) {
+		case ARCHON:
+			break;
+		case GUARD:
+			break;
+		case SCOUT:
+			break;
+		case SOLDIER:
+			break;
+		case TTM:
+			break;
+		case TURRET:
+			break;
+		case VIPER:
+			break;
+		default:
+			return null;
+		}
+		return null;
 	}
 	
+	/**
+	 * Static class!
+	 */
 	private RobotFactory() { }
 }
