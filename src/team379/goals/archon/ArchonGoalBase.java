1f938ee805e93ae3f21c993e3bba47ff5b18e8aa
Matt <MrZoraman@Gmail.com>
2016-01-22 19:41:45 -0700

    ACB cleaned up a bit

--- src/team379/goals/archon/ArchonGoalBase.java
+++ src/team379/goals/archon/ArchonGoalBase.java
@@ -1,29 +1,24 @@
 package team379.goals.archon;
 
 import battlecode.common.RobotController;
-import team379.Robot;
-import team379.RobotMemory;
 import team379.goals.Goal;
 import team379.signals.SignalData;
 import team379.signals.SignalType;
 
-public abstract class ArchonGoalBase extends Goal {
-
-	public ArchonGoalBase(RobotMemory memory) {
-		super(memory);
-	}
+public abstract class ArchonGoalBase implements Goal {
+	
+	private static double broadcastRadius = 10;		//TODO: magic number!
 
 	@Override
-	public Goal achieveGoal(RobotController rc, Robot robot) throws Exception {
+	public Goal achieveGoal(RobotController rc) throws Exception {
 		SignalData signalData = new SignalData(SignalType.THIS_IS_MY_ID, rc.getLocation(), (short) rc.getID());
 		int[] data = signalData.toInts();
-		rc.broadcastMessageSignal(data[0], data[1], memory.getBroadcastRadius());
+		int radiusSquared = (int) Math.pow(broadcastRadius, 2);
+		rc.broadcastMessageSignal(data[0], data[1], radiusSquared);
 		return null;
 	}
 	
-	protected void boostBroadcastRadius(int squares) {
-		double radius = Math.sqrt(memory.getBroadcastRadius());
-		radius += squares;
-		memory.setBroadcastRadius((int) Math.pow(radius, 2));
+	protected void boostBroadcastRadius(double squares) {
+		broadcastRadius += squares;
 	}
 }
