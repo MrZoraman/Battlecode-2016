927a6862baeecda173eebe0cd1f7f14d79330a30
Matt <MrZoraman@Gmail.com>
2016-01-22 18:41:33 -0700

    Created goal factory

---
+++ src/team379/goals/GoalFactory.java
@@ -0,0 +1,20 @@
+package team379.goals;
+
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+
+/**
+ * Creates a goal for a robot given the robot's type and the archon location.
+ * @author Matt
+ *
+ */
+public class GoalFactory {
+	/**
+	 * Static class!
+	 */
+	private GoalFactory() { }
+	
+	public static Goal createGoal(RobotController rc, MapLocation archonLocation) {
+		return null;
+	}
+}
