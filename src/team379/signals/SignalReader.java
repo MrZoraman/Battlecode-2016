43a27845c1284cd08ee79daca611c3d2b614e9be
Matt <MrZoraman@Gmail.com>
2016-01-22 18:47:45 -0700

    SignalReader can have multiple consumers

--- src/team379/signals/SignalReader.java
+++ src/team379/signals/SignalReader.java
@@ -4,7 +4,7 @@ import battlecode.common.RobotController;
 import battlecode.common.Signal;
 
 /**
- * Parses signals and sends them to a signal consumer
+ * Parses signals and sends them to signal consumers
  * @author Matt
  *
  */
@@ -15,11 +15,11 @@ public class SignalReader {
 	private SignalReader() { }
 	
 	/**
-	 * Emties the signal queue of a robot, parses the signals, then sends them to the consumer.
+	 * Empties the signal queue of a robot, parses the signals, then sends them to the consumers.
 	 * @param rc The RobotController
-	 * @param consumer The consumer that reads the signals.
+	 * @param consumer The consumers that read the signals.
 	 */
-	public static void consume(RobotController rc, SignalConsumer consumer) {
+	public static void consume(RobotController rc, SignalConsumer... consumers) {
 		//empty the signal queue
 		Signal[] signals = rc.emptySignalQueue();
 		for(int ii = 0; ii < signals.length; ii++) {
@@ -37,8 +37,10 @@ public class SignalReader {
 			
 			//parse the data
 			SignalData data = new SignalData(message[0], message[1]);
-			//send the data to the consumer
-			consumer.consume(data);
+			//send the data to the consumers
+			for(SignalConsumer sc : consumers) {
+				sc.consume(data);
+			}
 		}
 	}
 }
