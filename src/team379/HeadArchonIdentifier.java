5200beb7f83aa8399667883c7c4fd0d0a1e9ef2d
Matt <MrZoraman@Gmail.com>
2016-01-22 18:52:31 -0700

    HeadArchonIdentifier created.

---
+++ src/team379/HeadArchonIdentifier.java
@@ -0,0 +1,51 @@
+package team379;
+
+import battlecode.common.MapLocation;
+import team379.signals.SignalConsumer;
+import team379.signals.SignalData;
+import team379.signals.SignalType;
+
+/**
+ * Consumes signals and locates the head archon (the archon with the lowest id)
+ * @author Matt
+ *
+ */
+public class HeadArchonIdentifier implements SignalConsumer {
+
+	/**
+	 * The id of the archon.
+	 */
+	private int archonId = Integer.MAX_VALUE;
+	
+	/**
+	 * The location of the archon.
+	 */
+	private MapLocation archonLocation = null;
+	
+	@Override
+	public void consume(SignalData data) {
+		if(data.getType() == SignalType.THIS_IS_MY_ID) {
+			int id = data.getOtherInfo();
+			if(id < archonId) {
+				archonId = id;
+				archonLocation = data.getLocation();
+			}
+		}
+	}
+
+	/**
+	 * Gets the id of the head archon.
+	 * @return The head archon id.
+	 */
+	public int getArchonId() {
+		return archonId;
+	}
+	
+	/**
+	 * Gets the location of the head archon.
+	 * @return The head archon location.
+	 */
+	public MapLocation getArchonLocation() {
+		return archonLocation;
+	}
+}
