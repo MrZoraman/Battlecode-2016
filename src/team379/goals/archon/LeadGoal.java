8a2ad4db1bafa402c5d792f7d50a8c4df5ef0200
Matt <MrZoraman@Gmail.com>
2016-01-22 19:43:49 -0700

    Updated LeadGoal. StandardGoal no longer needed.

--- src/team379/goals/archon/LeadGoal.java
+++ src/team379/goals/archon/LeadGoal.java
@@ -1,19 +1,13 @@
 package team379.goals.archon;
 
 import battlecode.common.RobotController;
-import team379.Robot;
-import team379.RobotMemory;
 import team379.goals.Goal;
 
 public class LeadGoal extends ArchonGoalBase {
 
-	public LeadGoal(RobotMemory memory) {
-		super(memory);
-	}
-
 	@Override
-	public Goal achieveGoal(RobotController rc, Robot robot) throws Exception {
-		super.achieveGoal(rc, robot);
+	public Goal achieveGoal(RobotController rc) throws Exception {
+		super.achieveGoal(rc);
 		return null;
 	}
 
