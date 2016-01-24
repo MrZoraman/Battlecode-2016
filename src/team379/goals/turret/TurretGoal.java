8248816ff766bd239688283f79608676a85a7bec
Matt <MrZoraman@Gmail.com>
2016-01-24 14:45:16 -0700

    Turret always knows where archon is.

--- src/team379/goals/turret/TurretGoal.java
+++ src/team379/goals/turret/TurretGoal.java
@@ -12,18 +12,26 @@ import team379.pathfinding.PathFindResult;
 import team379.pathfinding.ArchonLocator;
 import team379.pathfinding.PathFinder;
 import team379.signals.SignalReader;
+import team379.signals.SignalType;
 import team379.signals.consumers.HeadArchonIdentifier;
 
 public class TurretGoal implements Goal{
 	private final PathFinder pf = new PathFinder();
 	
+	private static ArchonLocator al;
+	
 	@Override
 	public Goal achieveGoal(RobotController rc) throws Exception {
-		HeadArchonIdentifier hai = new HeadArchonIdentifier();
-		SignalReader.consume(rc, data ->{ 
-			
+		if(al == null) {
+			al = new ArchonLocator(RobotMemory.getArchonLocation(), RobotMemory.getArchonId());
+		}
+		SignalReader.consume(rc, data ->{
+			if(data.getType() == SignalType.THIS_IS_MY_ID) {
+				al.updateArchonLocation(data.getLocation());
+			}
 		});
-		MapLocation mp = RobotMemory.getArchonLocation();
+		
+		MapLocation mp = al.getArchonLocation(rc);
 		double archonDistance = ArchonDistance(mp);
 		
         return null;
