ecd5d3872c15d67217607ab0c7458b796604c3a0
Matt <MrZoraman@Gmail.com>
2016-01-26 01:15:48 -0700

    Turrets

--- src/team379/goals/archon/RobotFactory.java
+++ src/team379/goals/archon/RobotFactory.java
@@ -19,12 +19,12 @@ public class RobotFactory {
 		case 2: return GUARD;
 		case 3: return SOLDIER;
 		case 4: return GUARD;
-		case 5: return SOLDIER;
+		case 5: return TURRET;
 		case 6: return GUARD;
 		case 7: return SOLDIER;
 		case 8: return GUARD;
 		case 9: return SOLDIER;
-		case 10: return SCOUT;
+		case 10: return TURRET;
 		default: return GUARD;
 		}
 	}
